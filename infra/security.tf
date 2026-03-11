2ec50df34dfc0f56d96cbe910824b8e7494d0876
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2026-03-12 00:13:43 +0530

    Terraform base infra
    Created all files for basic infrastructure of ec2 instance.

---
+++ infra/security.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "instance_sg" {
+
+  name = "smallcase-devops-sg"
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Application Port"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
