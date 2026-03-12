55af153c61c8ba941aee65fae57c438861e639d5
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2026-03-12 17:58:14 +0530

    Add key for ec2
    Attached key for ec2 for secure ssh.

--- infra/main.tf
+++ infra/main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "smallcase_instance" {
 
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-
+  key_name      = var.key_pair_name
   associate_public_ip_address = true
 
   vpc_security_group_ids = [
--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,8 @@ variable "instance_type" {
   description = "EC2 instance type"
   default     = "t3.micro"
 }
+
+variable "key_pair_name" {
+  description = "Name of the existing AWS key pair to use for SSH access"
+  default     = "smallcase"
+}
